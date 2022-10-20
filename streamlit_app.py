b9f598014e82a7c0d4806081fcc0062e4ca6b333
HellyComet <hellyshah17@gmail.com>
2022-10-20 15:55:22 -0400

    Update streamlit_app.py
    Added FruityVice advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import requests
+
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -12,6 +14,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado","Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
